93f0c1719ffdb22b4bdc938018212b7becb50624
LENOVO <LENOVO@DESKTOP-L0CE2LE>
2023-02-09 19:13:35 +0530

    commit and push

---
+++ src/test/java/demooo/Practice.java
@@ -0,0 +1,22 @@
+package demooo;
+
+import org.testng.annotations.Test;
+
+public class Practice {
+	@Test
+	public void demoTest1() {
+		System.out.println("Demo Test- 1");
+	}
+   @Test
+	public void demoTest2() { 
+		System.out.println("Demo Test- 2");
+   }
+	@Test
+	public void demoTest3() {
+		System.out.println("Demo Test- 3");
+	}
+	@Test
+	public void demoTest4() {
+		System.out.println("Demo Test- 4");
+	}
+}
